32c94ac2ff97761bcdd6131baf920a2b032168f5
Lilac <lilacoren@gmail.com>
2015-09-10 15:40:09 +0000

    Finish Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ SampleApp::Application.routes.draw do
   
   match '/home', to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/home'
+      page.should have_content('Sample App')
+    end
+    
+    it "should have the right title 'Home'" do
+      visit '/home'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+  
+  describe "Help page" do
+    it "should have the content 'help'" do
+      visit '/help'
+      page.should have_content('Help')
+    end
+    
+    it "should have the h1 'Help'" do
+      visit '/help'
+      page.should have_selector('h1', :text => "Help")
+    end
+    
+    it "should have the right title 'Help'" do
+      visit '/help'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end 
+  
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/about'
+      page.should have_content('About Us')
+    end
+    
+    it "should have the h1 'About Us'" do
+      visit '/about'
+      page.should have_selector('h1', :text => "About Us")
+    end
+    
+    it "should have the right title 'About'" do
+      visit '/about'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+end
