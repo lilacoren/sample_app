3fa24c6c6e0a1ab0397f0afff5310fbdc96648c8
Lilac <lilacoren@gmail.com>
2015-09-20 12:38:17 +0000

    chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
+
   get "users/new"
   resources :users
   
@@ -14,6 +16,9 @@ SampleApp::Application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
